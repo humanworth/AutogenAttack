01bf644d412c4ab02dace810069ce4a27a2cf0ae
Ali Abbasi <ali.abbasi@thirdstream.ca>
2025-04-22 16:33:57 -0400

    starting the project

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from utils import get_openai_api_key
 OPENAI_API_KEY = get_openai_api_key()
 llm_config = {"model": "gpt-3.5-turbo"}
 
+from autogen import ConversableAgent
+
+agent = ConversableAgent(
+    name="chatbot",
+    llm_config=llm_config,
+    human_input_mode="NEVER",
+)
