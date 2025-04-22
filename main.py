ab0e11d23057fdccfc61b7157a54c58fb1b32075
Ali Abbasi <ali.abbasi@thirdstream.ca>
2025-04-22 16:03:49 -0400

    starting the project

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+from utils import get_openai_api_key
+OPENAI_API_KEY = get_openai_api_key()
+llm_config = {"model": "gpt-3.5-turbo"}
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hxcvxfi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
